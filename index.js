8f1abb45058a7e1795058532c641464e11310769
snail <1107554021@qq.com>
2018-08-21 16:29:44 +0800

    优化

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-// console
 ~(function() {
-    var methods = ['debug', 'error', 'info', 'log', 'warn', 'dir', 'dirxml', 'table', 'trace', 'assert', 'count', 'markTimeline', 'profile', 'profileEnd', 'time', 'timeEnd', 'timeStamp', 'timeline', 'timelineEnd', 'group', 'groupCollapsed', 'groupEnd', 'clear'];
-    var length = methods.length;
-    window.console = window.console || {};
-    window.log = console.log;
-
-    while (length--) {
-        console[methods[length]] = console[methods[length]] || function() {};
+    if (!window.console) {
+        var methods = ['debug', 'error', 'info', 'log', 'warn', 'dir', 'dirxml', 'table', 'trace', 'assert', 'count', 'markTimeline', 'profile', 'profileEnd', 'time', 'timeEnd', 'timeStamp', 'timeline', 'timelineEnd', 'group', 'groupCollapsed', 'groupEnd', 'clear'];
+        var length = methods.length;
+        var console = {};
+        
+        while (length--) {
+            console[methods[length]] = console[methods[length]] || function() {};
+        }
+        window.console = console;
     }
-})()
+    
+    window.log = console.log; // 项目中使用方便打印
+})();
